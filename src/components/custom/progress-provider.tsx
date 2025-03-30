24d079f79d3ed8b41d29d1e5e4debf70b7bd8709
sulemanahmedrajput <sulemanahmed2704@gmail.com>
2025-03-31 01:56:44 +0500

    added the loader

--- src/components/custom/progress-provider.tsx
+++ src/components/custom/progress-provider.tsx
@@ -1,18 +1,18 @@
-"use client";
+'use client';
 
-import { AppProgressBar as ProgressBar } from "next-nprogress-bar";
+import { AppProgressProvider } from '@bprogress/next';
 
 const ProgressProvider = ({ children }: { children: React.ReactNode }) => {
   return (
-    <>
+    <AppProgressProvider
+      height="6px"
+      color="hsl(173, 100%, 61%)"
+      options={{ showSpinner: true }}
+      shallowRouting
+      spinnerPosition='bottom-right'
+    >
       {children}
-      <ProgressBar
-        height="6px"
-        color="#4F46E5" // Use a solid color (e.g., Tailwind's Indigo-600)
-        options={{}}
-        shallowRouting
-      />
-    </>
+    </AppProgressProvider>
   );
 };
 
